405df9e2acf8fb95c500a035006624f5a99c03dc
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-05-16 17:30:53 -0500

    fix: :bug: arregle bug
    arregle  el bug del video ya que no lo mostraba

--- youtubePirata/main.js
+++ youtubePirata/main.js
@@ -1,3 +1,10 @@
+//agrego mi input y mi boton de buscar
+const buscar = document.querySelector('#btn');
+const inputBuscar = document.querySelector('#buscarVideo')
+let contenedorVideo = document.querySelector("#iframe");
+let creador = document.querySelector('#creador')
+let logoCreador = document.querySelector('#logo-creador')
+
 //  son las opciones que se usa para consumir los datos que trae la API
 const options = {
 	method: 'GET',
@@ -7,29 +14,21 @@ const options = {
 	}
 };
 
-//agrego mi input y mi boton de buscar
-const buscar = document.querySelector('#btn');
-const inputBuscar = document.querySelector('#buscarVideo').value
-
 //Agrego un evento de escucha al buscar, para que no me consuma tantos recursos de busqueda de la api y solo haga las cosas cuando hago click en el boton
 buscar.addEventListener('click',(e)=>{
     e.preventDefault();
-    let video = getVideo(inputBuscar)
-    mostrarVideo(video)
+    getVideo(inputBuscar.value)
 
 })
+
 //funcion para traerme los datos de la API
 //le paso el value que recibe el input para poder buscar el video que quiero por el id
 const getVideo = async(inputBuscar)=>{
     options.method = "GET";     
     let video = await ( await fetch(`https://youtube138.p.rapidapi.com/search/?q=${inputBuscar}&hl=en&gl=US`,options)).json();
-    return video
-}
+    //Video
+    contenedorVideo.src = `https://www.youtube.com/embed/${video.contents[0].video.videoId}`
+    //Especificaciones del canal
 
-function mostrarVideo(videos){
-    let contenedorVideo = document.querySelector("iframe");
-    contenedorVideo.src = `https://www.youtube.com/embed/${videos.contents[1].video.videoId}`
-    
 }
-
 
